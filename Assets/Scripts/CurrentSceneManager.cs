026a303ad8c3cf3e8acf5657af0dc4407c5f2b2a
benam-amin <benamaouche.amin@gmail.com>
2025-03-05 20:56:18 +0100

    Ajout d'un menu pause
    Utilisation d'un scriptable object pour créer le menu de pause, en réutilisant la logique du menu de mort

--- Assets/Scripts/CurrentSceneManager.cs
+++ Assets/Scripts/CurrentSceneManager.cs
@@ -6,37 +6,43 @@ public class CurrentSceneManager : MonoBehaviour
     public GameObject gameOverScreen;
     public GameObject playerMovement;
     public bool isPaused = false;
-
+    public GameObject pauseScreen; 
     public VoidEventChannel onPlayerDeath;
+    public VoidEventChannel onPause;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
 
     private void OnEnable()
     {
         onPlayerDeath.OnEventRaised += Die;    
+        onPause.OnEventRaised += Die;    
     }
 
     private void OnDisable()
     {
         onPlayerDeath.OnEventRaised -= Die;    
+        onPause.OnEventRaised -= Die;    
     }
 
     private void Die() {
         gameOverScreen.SetActive(true);
     }
 
-    private void Pause() {
+    public void Pause() {
         if (Time.timeScale == 0) {
                 Time.timeScale = 1;
                 isPaused = false;
                 playerMovement.GetComponent<PlayerMovement>().enabled = true;
+                pauseScreen.SetActive(false);
             } else {
                 Time.timeScale = 0;
                 isPaused = true;
                 playerMovement.GetComponent<PlayerMovement>().enabled = false;
+                pauseScreen.SetActive(true);
             }
     }
     // Update is called once per frame
